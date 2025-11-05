e95b7627b06e65af46fc1fa4e7bafcb88446fdc1
Luca Lorenzon <1104352+lucalorenzon@users.noreply.github.com>
2025-11-05 07:33:54 +0100

    fix: moved formula template here to avoid overwrite in tap repo, changed the replace task considering that know the file to change is already in this repo

---
+++ FormulaTemplate/brew-maintainer.rb
@@ -0,0 +1,31 @@
+class BrewMaintainer < Formula
+  desc "Automated Homebrew maintenance tool (update, upgrade, cleanup with logs)"
+  homepage "https://github.com/<REPLACED_BY_GITHUB_ACTION>"
+  version "<REPLACED_BY_GITHUB_ACTION>"
+  url "<REPLACED_BY_GITHUB_ACTION>"
+  sha256 "<REPLACED_BY_GITHUB_ACTION>"
+  license "MIT"
+
+  depends_on :macos # only macOS supported
+
+  def install
+    bin.install "brew-maintainer"
+  end
+
+  service do
+    run [opt_bin/"brew-maintainer"]
+    keep_alive true
+    log_path var/"log/brew-maintainer.log"
+    error_log_path var/"log/brew-maintainer.err.log"
+    working_dir var
+  end
+
+  def caveats
+    <<~EOS
+      brew-maintainer will automatically run every 6 hours via macOS service.
+      Logs are stored under:
+        #{var}/log/brew-maintainer.log
+      If a run requires user input, it will be skipped and noted in the log.
+    EOS
+  end
+end
